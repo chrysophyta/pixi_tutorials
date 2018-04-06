fbe037f61945c4405292531bb506dd4aab9b7bdc
Vivian <chrysophyta@gmail.com>
2018-04-06 03:47:31 -0400

    Creating the Pixi Application and stage

--- pixi_tutorial_1/index.js
+++ pixi_tutorial_1/index.js
@@ -4,3 +4,23 @@ if (!PIXI.utils.isWebGLSupported()) {
 }
 
 PIXI.utils.sayHello(type);
+
+//Create a Pixi Application
+//argument is a single object called the options
+let app = new PIXI.Application({
+  width: 256, // default: 800
+  height: 256, // default: 600
+  antialias: true, // default: false
+  transparent: false, // default: false
+  resolution: 1 // default: 1
+});
+console.log(app);
+
+//Add the canvas that Pixi automatically created for you to the HTML document
+document.body.appendChild(app.view);
+
+app.renderer.backgroundColor = 0x061639;
+app.renderer.view.style.position = 'absolute';
+app.renderer.view.style.display = 'block';
+app.renderer.autoResize = true;
+app.renderer.resize(window.innerWidth, window.innerHeight);
