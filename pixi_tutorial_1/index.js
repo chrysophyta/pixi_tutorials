885aa7a4cc28859f9c53319525a19c4d2f2db321
Vivian <chrysophyta@gmail.com>
2018-04-07 21:16:48 -0400

    added custom keyboard function

--- pixi_tutorial_1/index.js
+++ pixi_tutorial_1/index.js
@@ -9,6 +9,39 @@ PIXI.utils.sayHello(type);
 function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+// Keyboard Function
+function keyboard(keyCode) {
+  let key = {};
+  key.code = keyCode;
+  key.isDown = false;
+  key.isUp = true;
+  key.press = undefined;
+  key.release = undefined;
+  //The `downHandler`
+  key.downHandler = event => {
+    if (event.keyCode === key.code) {
+      if (key.isUp && key.press) key.press();
+      key.isDown = true;
+      key.isUp = false;
+    }
+    event.preventDefault();
+  };
+
+  //The `upHandler`
+  key.upHandler = event => {
+    if (event.keyCode === key.code) {
+      if (key.isDown && key.release) key.release();
+      key.isDown = false;
+      key.isUp = true;
+    }
+    event.preventDefault();
+  };
+
+  //Attach event listeners
+  window.addEventListener('keydown', key.downHandler.bind(key), false);
+  window.addEventListener('keyup', key.upHandler.bind(key), false);
+  return key;
+}
 
 //Create a Pixi Application
 //argument is a single object called the options
