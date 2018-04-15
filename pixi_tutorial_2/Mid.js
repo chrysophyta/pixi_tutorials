cb940ff3df805110d8b017cebf3fa7b309480725
Vivian <chrysophyta@gmail.com>
2018-04-14 22:18:45 -0700

    added mid.js and update method

---
+++ pixi_tutorial_2/Mid.js
@@ -0,0 +1,15 @@
+function Mid() {
+  var texture = PIXI.Texture.fromImage('resources/bg-mid.png');
+  PIXI.extras.TilingSprite.call(this, texture, 512, 256);
+
+  this.position.x = 0;
+  this.position.y = 128;
+  this.tilePosition.x = 0;
+  this.tilePosition.y = 0;
+}
+
+Mid.prototype = Object.create(PIXI.extras.TilingSprite.prototype);
+
+Mid.prototype.update = function() {
+  this.tilePosition.x -= 0.64;
+};
