d28b47b8595956f66e05045c5e4042e19fd7a37f
Vivian <chrysophyta@gmail.com>
2018-04-16 22:35:02 -0700

    load sprite sheet

--- pixi_tutorial_2/Main.js
+++ pixi_tutorial_2/Main.js
@@ -3,15 +3,25 @@ function Main() {
   this.renderer = PIXI.autoDetectRenderer(512, 384, {
     view: document.getElementById('game-canvas')
   });
-
-  this.scroller = new Scroller(this.stage);
-
-  requestAnimationFrame(this.update.bind(this));
+  this.loadSpriteSheet();
 }
 Main.SCROLL_SPEED = 5;
 
 Main.prototype.update = function() {
   this.scroller.moveViewportXBy(Main.SCROLL_SPEED);
   this.renderer.render(this.stage);
+  requestAnimationFrame(this.update.bind(this));
+};
+Main.prototype.loadSpriteSheet = function() {
+  var loader = PIXI.loader;
+  loader.add('wall', 'resources/wall.json');
+  loader.add('bg-mid', 'resources/bg-mid.png');
+  loader.add('bg-far', 'resources/bg-far.png');
+  loader.once('complete', this.spriteSheetLoaded.bind(this));
+  loader.load();
+};
+
+Main.prototype.spriteSheetLoaded = function() {
+  this.scroller = new Scroller(this.stage);
   requestAnimationFrame(this.update.bind(this));
 };
