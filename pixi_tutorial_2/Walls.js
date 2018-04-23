7fc2aced96c30068a32a44fa8b8eeee539ee72a6
Vivian <chrysophyta@gmail.com>
2018-04-22 22:32:34 -0700

    addNewSlice and removeOldSlices

--- pixi_tutorial_2/Walls.js
+++ pixi_tutorial_2/Walls.js
@@ -20,6 +20,8 @@ Walls.prototype.setViewportX = function(viewportX) {
   this.viewportX = this.checkViewportXBounds(viewportX);
   var prevViewportSliceX = this.viewportSliceX;
   this.viewportSliceX = Math.floor(this.viewportX / WallSlice.WIDTH);
+  this.removeOldSlices(prevViewportSliceX);
+  this.addNewSlices();
 };
 Walls.prototype.checkViewportXBounds = function(viewportX) {
   var maxViewportX =
@@ -31,6 +33,42 @@ Walls.prototype.checkViewportXBounds = function(viewportX) {
   }
 
   return viewportX;
+};
+Walls.prototype.addNewSlices = function() {
+  var firstX = -(this.viewportX % WallSlice.WIDTH);
+  for (
+    var i = this.viewportSliceX, sliceIndex = 0;
+    i < this.viewportSliceX + Walls.VIEWPORT_NUM_SLICES;
+    i++, sliceIndex++
+  ) {
+    var slice = this.slices[i];
+    if (slice.sprite == null && slice.type != SliceType.GAP) {
+      slice.sprite = this.borrowWallSprite(slice.type);
+
+      slice.sprite.position.x = firstX + sliceIndex * WallSlice.WIDTH;
+      slice.sprite.position.y = slice.y;
+
+      this.addChild(slice.sprite);
+    } else if (slice.sprite != null) {
+      slice.sprite.position.x = firstX + sliceIndex * WallSlice.WIDTH;
+    }
+  }
+};
+
+Walls.prototype.removeOldSlices = function(prevViewportSliceX) {
+  var numOldSlices = this.viewportSliceX - prevViewportSliceX;
+  if (numOldSlices > Walls.VIEWPORT_NUM_SLICES) {
+    numOldSlices = Walls.VIEWPORT_NUM_SLICES;
+  }
+
+  for (var i = prevViewportSliceX; i < prevViewportSliceX + numOldSlices; i++) {
+    var slice = this.slices[i];
+    if (slice.sprite != null) {
+      this.returnWallSprite(slice.type, slice.sprite);
+      this.removeChild(slice.sprite);
+      slice.sprite = null;
+    }
+  }
 };
 
 Walls.prototype.createLookupTables = function() {
